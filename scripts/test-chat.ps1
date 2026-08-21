30741c907c9864b177be54e0413b71c5915884e4
William M. Berger <103152537+berger33@users.noreply.github.com>
2026-08-21 11:18:13 -0300

    chore(cleanup): remove obsolete FastAPI chat smoke test

--- scripts/test-chat.ps1
+++
@@ -1,7 +0,0 @@
-$body = @{
-  message = 'Explique em poucas linhas como este sistema funciona.'
-  chat_id = 'local-test'
-  use_web = $false
-} | ConvertTo-Json
-
-Invoke-RestMethod -Method Post -Uri 'http://localhost:8000/chat' -ContentType 'application/json' -Body $body | ConvertTo-Json -Depth 8
