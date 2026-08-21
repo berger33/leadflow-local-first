bb1b3a33f4f9bbaedfb838808222b0a327fbc2a9
William M. Berger <103152537+berger33@users.noreply.github.com>
2026-08-21 11:18:31 -0300

    chore(cleanup): remove obsolete research smoke test

--- scripts/test-research.ps1
+++
@@ -1,9 +0,0 @@
-$body = @{
-  query = '10 notícias mais relevantes de inteligência artificial e tecnologia nas últimas 24 horas'
-  limit = 10
-  kind = 'news'
-  timelimit = 'd'
-  language = 'pt-BR'
-} | ConvertTo-Json
-
-Invoke-RestMethod -Method Post -Uri 'http://localhost:8000/research' -ContentType 'application/json' -Body $body | ConvertTo-Json -Depth 10
